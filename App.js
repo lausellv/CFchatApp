c9289c5fc548b98419681b25353a069cda01307a
Victor <lausellv@yahoo.com>
2021-09-04 14:08:18 +0200

    CF 5.1

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-("react");
 import { Alert, Button, StyleSheet, TextInput, View, Text } from "react-native";
 
 export default class App extends Component {

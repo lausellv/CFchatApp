d1f016e6b86bf844b1a13c0dc8d5c729ba2e39ff
Victor <lausellv@yahoo.com>
2021-09-04 13:19:33 +0200

    5.1 text input and button

--- App.js
+++ App.js
@@ -1,25 +1,31 @@
 import React, { Component } from "react";
 ("react");
-import { StyleSheet, TextInput, View } from "react-native";
+import { Alert, Button, StyleSheet, TextInput, View, Text } from "react-native";
 
-export default class HelloWorld extends Component {
+export default class App extends Component {
+  state = { text: "" };
+
+  alertMyText(input = []) {
+    Alert.alert(input.text);
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.box1}></View>
-        <View style={styles.box2}></View>
-        <View style={styles.box3}></View>
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <TextInput
+          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+          onChangeText={text => this.setState({ text })}
+          value={this.state.text}
+          placeholder="Type here ..."
+        ></TextInput>
+        <Text>You wrote: {this.state.text}</Text>
+        <Button
+          onPress={() => {
+            this.alertMyText({ text: this.state.text });
+          }}
+          title="Press Me"
+        />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // flexDirection: "column" by default
-   },
-  box1: { flex: 1, backgroundColor: "#efb5a3" },
-  box2: { flex: 2, backgroundColor: "#f57e7e" },
-  box3: { flex: 3, backgroundColor: "#315f72" }
-});

d2162658361c052d5062012f36a7451b035effe4
Victor <lausellv@yahoo.com>
2021-09-04 14:37:17 +0200

    5.1 implementing React Native Navigation

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
-import { ScrollView, Alert, Button, StyleSheet, TextInput, View, Text } from "react-native";
+// import react native gesture handler
+import 'react-native-gesture-handler';
 
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { ScrollView, Alert, Button, StyleSheet, TextInput, View, Text } from "react-native";
+import Screen1 from './components/Screen1';
+import Screen2 from './components/Screen2';
+
+
+// Create the navigator
+const Stack = createStackNavigator();
 export default class App extends Component {
   state = { text: "" };
 
@@ -10,21 +21,20 @@ export default class App extends Component {
 
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center" }}>
-        <TextInput
-          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-          onChangeText={text => this.setState({ text })}
-          value={this.state.text}
-          placeholder="Type here ..."
-        ></TextInput>
-        <Text>You wrote: {this.state.text}</Text>
-        <Button
-          onPress={() => {
-            this.alertMyText({ text: this.state.text });
-          }}
-          title="Press Me"
+      <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Screen1"
+      >
+        <Stack.Screen
+          name="Screen1"
+          component={Screen1}
         />
-      </View>
+        <Stack.Screen
+          name="Screen2"
+          component={Screen2}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
     );
   }
 }

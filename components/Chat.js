f63974e09689d33cfd8713f43d9798a3fe6a3534
Victor <lausellv@yahoo.com>
2021-09-05 14:26:58 +0200

    5.1 destructured name, backgColor before return

--- components/Chat.js
+++ components/Chat.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
 
 export default class Chat extends React.Component {
-
   render() {
-    let name = this.props.route.params.name; // OR ...
-    // let { name } = this.props.route.params;
+    //let name = this.props.route.params.name; // OR ...
+    let { name, backgroundColor } = this.props.route.params;
 
     this.props.navigation.setOptions({ title: name });
 
     return (
-      <View style={{ flex:1, backgroundColor: this.props.route.params.backgroundColor}}>
+      <View style={{ flex: 1, backgroundColor: backgroundColor }}>
         {/* Rest of the UI */}
-        <Text style={{ fontSize: 26,
-    fontWeight: "600",
-    color: "#FFFFFF",
-    textAlign: "center",
-    lineHeight: 60,
-    paddingTop: 90}}>Hello {name}</Text>
+        <Text
+          style={{
+            fontSize: 26,
+            fontWeight: "600",
+            color: "#FFF",
+            textAlign: "center",
+            lineHeight: 60,
+            paddingTop: 90
+          }}
+        >
+          Hello {name}
+        </Text>
       </View>
     );
-  };
+  }
 }
